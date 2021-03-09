d434f05102bd613bcfdeb5efac8a7e7d634ccaaf
hiteshranaut <hiteshranaut92@gmail.com>
2021-03-09 13:09:48 +0530

    advanced changes done on search js

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,58 +4,53 @@ import axios from 'axios';
 
 const Search = () => {
     const [ term , setTerm] = useState('programming');
+    const [debouncedTerm , setDebouncedTerm] = useState(term);
     const [results , setResults] = useState([]);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        } , 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+
+    } , [term]);
+
+    useEffect(() => {
+
+        const search = async () => {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php' , {
+                 params: {
+                     action: 'query' ,
+                     list: 'search' ,
+                     origin: "*" ,
+                     format: 'json' ,
+                     srsearch: debouncedTerm 
+                 }
+             });
+ 
+             setResults(data.query.search);
+ 
+ 
+         };
+
+         search();
+
+    } , [debouncedTerm])
     
-    console.log(results);
     //[] => run on init render
     // nother => run on init and every render
     // [value] = run on every time value change     
-    useEffect(() => {
-        const search = async () => {
-           const {data} = await axios.get('https://en.wikipedia.org/w/api.php' , {
-                params: {
-                    action: 'query' ,
-                    list: 'search' ,
-                    origin: "*" ,
-                    format: 'json' ,
-                    srsearch: term 
-                }
-            });
-
-            setResults(data.query.search);
-
-
-        }
     
-        if(term && !results.length) {
-            search();
-        } else {
-
-            const timeoutId = setTimeout(() => {
-                if(term){
-                    search();
-                }
-            }, 500);
-                // console.log("changed")
-                // this will not run on first run 
-                // this will only run when element rerender second time
-                return () => {
-                    clearTimeout(timeoutId);
-                }
-                
-
-        }
-
-  
-        
-    }, [term]);
 
 
     const renderedResults = results.map((result) => {
         return (
             <div key={result.pageid} className="item">
                 <div className="right floated content">
-                    <a target="_blank" href={`https://en.wikipedia.org?curid=${result.pageid}`} className="ui button">Go</a>
+                    <a rel="noreferrer" target="_blank" href={`https://en.wikipedia.org?curid=${result.pageid}`} className="ui button">Go</a>
                 </div>
                 <div className="content">
                     <div className="header">
